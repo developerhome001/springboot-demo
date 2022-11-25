b0fa13e25dbf1bb8a22aecd10acc2d8067a2b90e
keray <1172304645@qq.com>
2022-11-25 17:38:57 +0800

    ServletInvocableHandlerMethodHandler注入问题

--- common-boot-web/src/main/java/com/keray/common/handler/KerayWebMvcRegistrations.java
+++ common-boot-web/src/main/java/com/keray/common/handler/KerayWebMvcRegistrations.java
@@ -1,44 +1,44 @@
 package com.keray.common.handler;
 
-import cn.hutool.core.collection.CollUtil;
 import com.keray.common.ServletInvocableHandlerMethodFactory;
 import com.keray.common.ServletInvocableHandlerMethodHandler;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.util.ProxyUtils;
 import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Comparator;
+import java.util.LinkedList;
 
 @Configuration(proxyBeanMethods = false)
 @Primary
-public class KerayWebMvcRegistrations implements WebMvcRegistrations {
+public class KerayWebMvcRegistrations implements WebMvcRegistrations, BeanPostProcessor {
 
 
-    ServletInvocableHandlerMethodHandler[] handlers = null;
+    private ServletInvocableHandlerMethodHandler[] handlers = new ServletInvocableHandlerMethodHandler[]{};
 
     @Resource
     private ServletInvocableHandlerMethodFactory servletInvocableHandlerMethodFactory;
 
-    @Resource
-    private ApplicationContext applicationContext;
 
-    @PostConstruct
-    public void initHandler() {
-        Collection<ServletInvocableHandlerMethodHandler> handlerMethodHandlers = applicationContext
-                .getBeansOfType(ServletInvocableHandlerMethodHandler.class).values();
-        if (CollUtil.isNotEmpty(handlerMethodHandlers)) {
-            ServletInvocableHandlerMethodHandler[] array = handlerMethodHandlers.toArray(new ServletInvocableHandlerMethodHandler[]{});
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        var clazz = ProxyUtils.getUserClass(bean);
+        if (ServletInvocableHandlerMethodHandler.class.isAssignableFrom(clazz)) {
+            var list = new LinkedList<>(Arrays.asList(handlers));
+            list.add((ServletInvocableHandlerMethodHandler) bean);
+            var array = list.toArray(new ServletInvocableHandlerMethodHandler[]{});
             Arrays.sort(array, Comparator.comparing(ServletInvocableHandlerMethodHandler::getOrder));
             handlers = array;
         }
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
     }
 
     @Override
