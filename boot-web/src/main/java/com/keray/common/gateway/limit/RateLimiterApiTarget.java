a6f481fdb138252437b251df203a81002a2ab968
keray <1172304645@qq.com>
2023-02-01 10:26:27 +0800

    注释

--- boot-web/src/main/java/com/keray/common/gateway/limit/RateLimiterApiTarget.java
+++ boot-web/src/main/java/com/keray/common/gateway/limit/RateLimiterApiTarget.java
@@ -4,7 +4,7 @@ package com.keray.common.gateway.limit;
  * 接口令牌桶限定对象
  */
 public enum RateLimiterApiTarget {
-    namespace, // 空间名限定
-    ip,// ip限定
+    namespace, // 空间名限定 空间名限定时表示全局限制这个接口，比如一个接口只能允许10QPS/秒
+    ip,// ip限定    ip限制表示
     user, // 用户id限定
 }
