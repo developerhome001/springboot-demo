819ff86419b2fe0c95934cb9932deca1628e2cd6
keray <1172304645@qq.com>
2023-05-17 13:25:26 +0800

    注释

--- boot/src/main/java/com/keray/common/SystemProperty.java
+++ boot/src/main/java/com/keray/common/SystemProperty.java
@@ -17,7 +17,7 @@ public class SystemProperty {
 
 
     /**
-     * header头表示appid的key
+     * MemoryRateLimiterStore内存流控最大支持的key  大于后流控数据被清空重新计数
      */
     public static final String MEMORY_RATE_MAX_LEN = "MEMORY_RATE_MAX_LEN";
     /**
