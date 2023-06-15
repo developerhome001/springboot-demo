8af628e5abf4aecc2ae69ce43c003504b9a20751
keray <1172304645@qq.com>
2023-06-15 16:57:55 +0800

    update

--- boot/src/main/java/com/keray/common/qps/spring/RateLimiterBeanAutoConfig.java
+++ boot/src/main/java/com/keray/common/qps/spring/RateLimiterBeanAutoConfig.java
@@ -1,21 +1,18 @@
-package com.keray.common.gateway.limit;
+package com.keray.common.qps.spring;
 
 import com.keray.common.lock.RedissonLock;
-import com.keray.common.qps.spring.MemoryRateLimiterBean;
-import com.keray.common.qps.spring.RateLimiterBean;
-import com.keray.common.qps.spring.RedisRateLimiterBean;
+import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnClass(RedisTemplate.class)
 public class RateLimiterBeanAutoConfig {
+
+    @ConditionalOnClass(RedissonAutoConfiguration.class)
     @Bean
-    @ConditionalOnBean(name = "redisTemplate")
     public RateLimiterBean<?> redisRateLimiterBean(RedissonLock redissonLock, @Qualifier("redisTemplate") RedisTemplate<String, String> redisTemplate) {
         return new RedisRateLimiterBean(redissonLock, redisTemplate);
     }
